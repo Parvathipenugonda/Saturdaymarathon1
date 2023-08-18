24a2cda776438b79cbe6422ddff641fa82b169d2
Parvathipenugonda <parvathipenugonda26195@gmail.com>
2023-08-18 16:14:34 +0530

    pattern
    practice pattern based programs

---
+++ KodnestLevel4/src/Pattern.java
@@ -0,0 +1,21 @@
+
+public class Pattern {
+public static void main(String[] args) {
+	for(int i=1;i<=2;i++)
+	{
+		for(int j=1;j<=10;j++)
+		{
+			if(j<6)
+			{
+				System.out.print("$");
+			}
+			else
+			{
+				System.out.print("*");
+			}
+			
+		}
+		
+	}
+}
+}
---
+++ kodnestlevel2/src/kodnestlevel2/Demo.java
@@ -0,0 +1,10 @@
+package kodnestlevel2;
+
+public class Demo {
+public static void main(String[] args) {
+	for(int i=1;i<=5;i++)
+	{
+		System.out.print(i);
+	}
+}
+}
